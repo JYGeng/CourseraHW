2ffcfe5560881e470820683ee59c7ebb3e6bb4e5
JYGeng <878962665@qq.com>
2016-05-15 14:43:14 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,16 @@
 makeCacheMatrix <- function(x = matrix()) {
-+     m <- NULL
-+     set <- function(y) {
-+         x <<- y
-+         m <<- NULL
-+     }
-+     get <- function() x
-+     setinv <- function(solve) m <<- inv
-+     getinv <- function() m
-+     list(set = set, get = get,
-+          setinv = setinv,
-+          getinv = getinv)
-+ }  
+    m <- NULL
+     set <- function(y) {
+         x <<- y
+         m <<- NULL
+              }
+     get <- function() x
+     setinv <- function(solve) m <<- inv
+     getinv <- function() m
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
+ }  
 
 cacheSolve <- function(x, ...) {
         m <- x$getinv()

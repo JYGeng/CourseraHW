83c258497fce8c90a9f60357f417d1ce00cdf27f
JYGeng <878962665@qq.com>
2016-05-15 14:54:33 +0800

    coursera HW

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@ makeCacheMatrix <- function(x = matrix()) {
          m <<- NULL
               }
      get <- function() x
-     setinv <- function(solve) m <<- inv
+     setinv <- function(inv) m <<- inv
      getinv <- function() m
      list(set = set, get = get,
           setinv = setinv,
